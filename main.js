e03df3cfe27ecd209fb6d44bd4671006de77849c
Eldar <kadrileevworkmail@gmail.com>
2025-07-07 14:31:43 +0500

    final_fr

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ a = +prompt('Введите число')
 b = +prompt('Введите степень')
 c = a;
 
-for(let i = 1; i < b; i++){
-    c*=a;
+if(b == 0){
+    c = 1;
+}
+else{
+    for(let i = 1; i < b; i++){
+        c*=a;
+    }
 }
 
 alert('Результат: ' + c)
